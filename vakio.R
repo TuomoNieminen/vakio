0a0d7882f29e3e140e45e3a35ba9579d3dea12d6
TuomoNieminen <tuomo.a.nieminen@helsinki.fi>
2016-10-24 00:37:48 +0300

    moved part of vakio.R to prob_sim.R

--- vakio.R
+++ vakio.R
@@ -1,7 +1,7 @@
+# Aapeli Nevala 10/2016
 
 rivit <- read.csv2("Tilastot.csv", stringsAsFactors = FALSE)
 colnames(rivit) <- 1:13
-
 
 x = numeric(nrow(rivit))
 for (i in 1:nrow(rivit)) {
@@ -13,13 +13,3 @@ for (i in 1:nrow(rivit)) {
   }
 }
 
-# simuloidaan P(kolme tai yli samaa riviä)
-riveja <- 1:(3^13)
-simsize <- 1e5
-n <- nrow(rivit)
-
-df <- matrix(NA, nrow = simsize, ncol = n)
-for(i in 1:simsize) df[i, ] <- sample(riveja, size = n, replace = T)
-
-results <- apply(df, 1, FUN = function(v) any(table(v)>2))
-sum(results)/length(results)
